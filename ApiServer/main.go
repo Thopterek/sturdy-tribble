1c696f20da551567511a1577719747c36779b513
itsiros <jtsiros93@gmail.com>
2026-06-05 12:43:20 +0200

    Creating connection with DB posible

--- ApiServer/main.go
+++ ApiServer/main.go
@@ -65,7 +65,8 @@ func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
-	cfg := loadEnvAndConnect()
+	//cfg := loadEnvAndConnect()
+	cfg := apiConf{} // Temporary fix until DB is connected and configured
 	mux := http.NewServeMux()
 
 	fsHandler := cfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
