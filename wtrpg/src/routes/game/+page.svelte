06e9a6b53b1f648e850b09bd1b1ee364675eebd6
Thopterek <nataniel.dziadzia@gmail.com>
2026-06-12 02:38:06 +0200

    Tried to play around with SVG
    as for now it's like that later on the plan is to recreate some of the
    components per hand rather than having them being done through some
    converters as they are creating a lot of bloated code that doesn't
    really make any sense

--- wtrpg/src/routes/game/+page.svelte
+++ wtrpg/src/routes/game/+page.svelte
@@ -8,7 +8,11 @@
 <svelte:document {onselectionchange} />
 
 <h1>Game Lobby</h1>
-
+<svg width="100" height="100" viewBox="0 0 100 100">
+	<rect x="10" y="10" width="50" height="30" fill="red" />
+	<circle cx="50" cy="50" r="20" fill="blue" />
+	<line x1="0" y1="0" x2="100" y2="100" stroke="black" stroke-width="2" />
+</svg>
 <button class="button">
 	<a href="/dashboard">Check that profile</a>
 </button>
