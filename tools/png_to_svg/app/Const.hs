49335a713050ba70c908a22d9849b53bcdc148e8
Thopterek <nataniel.dziadzia@gmail.com>
2026-06-20 20:56:31 +0200

    Slight modification, escaping the quotetion marks for the const strings
    Also changed cabal file that actually now the LSP and all the NEOVIM
    setup is working (thank you Stelio) that I could write this beautifull
    Haskell code (I love pain and I did recursion in minishell and I want to
    suffer once again but on another level, life is a crude adventure into
    the mind of a mad god)

--- tools/png_to_svg/app/Const.hs
+++ tools/png_to_svg/app/Const.hs
@@ -6,26 +6,41 @@ path = "../../overview/pixel_art_sketches/"
 
 -- The text for box that differes per the colour
 black :: String
-black = "outline"
+black = "\"outline\""
 
 -- Couple that to be used later (not in d6 / d20)
 darkspot :: String
-darkspot = "darkspot"
+darkspot = "\"darkspot\""
+
 between :: String
-between = "between"
+between = "\"between\""
 
 -- Back to text used before
 shadow :: String
-shadow = "shadow"
+shadow = "\"shadow\""
+
 slight :: String
-slight = "small_light"
+slight = "\"small_light\""
+
 gbase :: String
-gbase = "grey_base"
+gbase = "\"grey_base\""
+
 white :: String
-white = "inner_white"
+white = "\"inner_white\""
+
+-- Hardcoded data about making SVG
+start :: String
+start = "<svg width=\"64\" height=\"64\" viewBox=\"0 0 16 16\">"
+
+end :: String
+end = "</svg>"
 
 -- Making an rectangle
 recStart :: String
 recStart = "<rect class="
+
 recEnd :: String
 recEnd = "/>"
+
+-- left most corner on top x 0 y 0, width grows right, height grows down
+data Rectangle = Rectangle {x :: Int, y :: Int, width :: Int, height :: Int, color :: String}
--- tools/png_to_svg/app/Main.hs
+++ tools/png_to_svg/app/Main.hs
@@ -7,21 +7,22 @@ import System.FilePath ((</>))
 -- Way to classify all of the colours used in Pixel Arts
 classification :: PixelRGB8 -> String
 classification (PixelRGB8 r g b)
-    | r < 40 && g < 40 && b < 40 = black
-    | r == 49 && g == 49 && b == 49 = darkspot
-    | r == 84 && g == 85 && b == 85 = between
-    | r == 121 && g == 120 && b == 120 = shadow
-    | r == 161 && g == 160 && b == 160 = slight
-    | r == 213 && g == 212 && b == 212 = gbase
-    | r > 233 && g > 233 && b > 233 = white
+  | r < 40 && g < 40 && b < 40 = black
+  | r == 49 && g == 49 && b == 49 = darkspot
+  | r == 84 && g == 85 && b == 85 = between
+  | r == 121 && g == 120 && b == 120 = shadow
+  | r == 161 && g == 160 && b == 160 = slight
+  | r == 213 && g == 212 && b == 212 = gbase
+  | r > 233 && g > 233 && b > 233 = white
+  | otherwise = "\"ErrorBruh\""
 
 -- Haskell, why are you using tabs instead of spaces :c
 main :: IO ()
 main = do
-    result <- readImage (path </> "bird.png")
-    case result of
-        Left err -> putStrLn $ "Error: " ++ err
-        Right img -> do
-            let w = dynamicMap imageWidth img
-            let h = dynamicMap imageHeight img
-            putStrLn $ "Width: " ++ show w ++ ", Height: " ++ show h
+  result <- readImage (path </> "bird.png")
+  case result of
+    Left err -> putStrLn $ "Error: " ++ err
+    Right img -> do
+      let w = dynamicMap imageWidth img
+      let h = dynamicMap imageHeight img
+      putStrLn $ "Width: " ++ show w ++ ", Height: " ++ show h
