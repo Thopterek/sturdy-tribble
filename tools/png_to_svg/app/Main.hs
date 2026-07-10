23d73c68c3efc694db38e87b8a972f59dace4d08
Thopterek <nataniel.dziadzia@gmail.com>
2026-07-10 12:55:40 +0200

    Tried to fix dependency error, which was actually a syntax error
    Reading the output of the LSP is weird, that's it.
    
    Back to actually working on the project rather than figuring out the
    mess

--- tools/png_to_svg/app/Main.hs
+++ tools/png_to_svg/app/Main.hs
@@ -7,23 +7,23 @@ import System.FilePath ((</>))
 -- Way to classify all of the colours used in Pixel Arts
 classification :: PixelRGB8 -> String
 classification (PixelRGB8 r g b)
-  | r < 40 && g < 40 && b < 40 = black
-  | r == 49 && g == 49 && b == 49 = darkspot
-  | r == 84 && g == 85 && b == 85 = between
-  | r == 121 && g == 120 && b == 120 = shadow
-  | r == 161 && g == 160 && b == 160 = slight
-  | r == 213 && g == 212 && b == 212 = gbase
-  | r > 233 && g > 233 && b > 233 = white
-  | otherwise = "\"ErrorBruh\""
+    | r < 40 && g < 40 && b < 40 = black
+    | r == 49 && g == 49 && b == 49 = darkspot
+    | r == 84 && g == 85 && b == 85 = between
+    | r == 121 && g == 120 && b == 120 = shadow
+    | r == 161 && g == 160 && b == 160 = slight
+    | r == 213 && g == 212 && b == 212 = gbase
+    | r > 233 && g > 233 && b > 233 = white
+    | otherwise = "\"ErrorBruh\""
 
 -- Main caller? To make it like in C it would be
 -- int writeRectangles(Image img, int w, int h)
 -- where is for local variables and the recursion?!
-writeRectangles :: Image PixelRGB8 -> Int -> Int
-writeRectangles total = rec img w h
-  where
-    total = h * w
-    rec img  w h
+-- writeRectangles :: Image PixelRGB8 -> Int -> Int
+-- writeRectangles total = rec img w h
+--  where
+--    total = h * w
+--    rec img  w h
 
 -- Haskell, why are you using tabs instead of spaces :c
 -- plus very random but the idea is: return value is the fn
@@ -32,11 +32,11 @@ writeRectangles total = rec img w h
 -- skrr x = x * x
 main :: IO ()
 main = do
-  result <- readImage (path </> "bird.png")
-  case result of
-    Left err -> putStrLn $ "Error: " ++ err
-    Right img -> do
-      let w = dynamicMap imageWidth img
-      let h = dynamicMap imageHeight img
-      writeRectangles img w h
-      putStrLn $ "Width: " ++ show w ++ ", Height: " ++ show h
+    result <- readImage (path </> "bird.png")
+    case result of
+        Left err -> putStrLn $ "Error: " ++ err
+        Right img -> do
+            let w = dynamicMap imageWidth img
+            let h = dynamicMap imageHeight img
+            -- writeRectangles img w h
+            putStrLn $ "Width: " ++ show w ++ ", Height: " ++ show h
