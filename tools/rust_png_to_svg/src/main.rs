4114d84cc2328f6fb9fc6aa6b4d2dbd835d6edb7
Thopterek <nataniel.dziadzia@gmail.com>
2026-08-03 20:44:47 +0200

    Coming back is hard but we gonna get there Stelio
    .        +          .      .          .
         .            _        .                    .
      ,              /;-._,-.____        ,-----.__
     ((        .    (_:#::_.:::. `-._   /:, /-._, `._,
      `                 \   _|`"=:_::.`.);  \ __/ /
                          ,    `./  \:. `.   )==-'  .
        .      ., ,-=-.  ,\, +#./`   \:.  / /           .
    .           \/:/`-' , ,\ '` ` `   ): , /_  -o
           .    /:+- - + +- : :- + + -:'  /(o-) \)     .
      .      ,=':  \    ` `/` ' , , ,:' `'--".--"---._/`7
       `.   (    \: \,-._` ` + '\, ,"   _,--._,---":.__/
                  \:  `  X` _| _,\/'   .-'
    .               ":._:`\____  /:'  /      .           .
                        \::.  :\/:'  /              +
       .                 `.:.  /:'  }      .
               .           ):_(:;   \           .
                          /:. _/ ,  |
                       . (|::.     ,`                  .
         .                |::.    {\
                          |::.\  \ `.
                          |:::(\    |
                  O       |:::/{ }  |                  (o
                   )  ___/#\::`/ (O "==._____   O, (O  /`
              ~~~w/w~"~~,\` `:/,-(~`"~~~~~~~~"~o~\~/~w|/~
    dew   ~~~~~~~~~~~~~~~~~~~~~~~\\W~~~~~~~~~~~~\|/~~

--- tools/rust_png_to_svg/src/main.rs
+++ tools/rust_png_to_svg/src/main.rs
@@ -1,3 +1,7 @@
+use std::fs::read;
+
 fn main() {
     println!("Hello, we are changing this dummy from Haskell!");
+    let mut file = read("../../../overview/pixel_art_sketches/bird.png");
+    // println!(file);
 }
