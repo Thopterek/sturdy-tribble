afd4405722bd8de3271a3cf9c6a1708475d1d9db
Thopterek <nataniel.dziadzia@gmail.com>
2026-08-04 23:17:31 +0200

    Boy is it a mess and a bit harder than expected, thank you Stelio for guidance
    kids remember gd works also to find the implementation in the libraries
    you are using and there is nothing wrong with fighting against language
    that you have for 3 months straight just to forget about it in the mean
    time

--- tools/rust_png_to_svg/src/main.rs
+++ tools/rust_png_to_svg/src/main.rs
@@ -1,7 +1,22 @@
-use std::fs::read;
+use image::{GenericImageView, Pixel, Rgba};
 
 fn main() {
-    println!("Hello, we are changing this dummy from Haskell!");
-    let mut file = read("../../../overview/pixel_art_sketches/bird.png");
-    // println!(file);
+    let path_to_img = "../../overview/pixel_art_sketches/bird.png";
+    let res = image::open(path_to_img);
+    if let Ok(img) = res {
+        let dimensions = img.dimensions();
+        println!("Dimensions {:?}", img.dimensions());
+        if dimensions.0 != 16 && dimensions.1 != 16 {
+            println!("Error: Wrong image my guy, here we work on 16 by 16");
+            println!("Info: Should it be an error? No. Do I care? Not enough");
+            return;
+        }
+        for one in img.pixels() {
+            let r = one.2;
+            r.to_rgb();
+            // r.map();
+        }
+    } else {
+        println!("Error: It uses relative path to find image {}", path_to_img);
+    }
 }
