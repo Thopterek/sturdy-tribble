0c232fad03ef1c5e46476139fcb84cccb96b726f
Thopterek <nataniel.dziadzia@gmail.com>
2026-08-09 20:59:51 +0200

    tldr: The problem is that I am just checking the first R value of the pixel
    So Stelio, listen up. It would be working fine but I have to figure out
    what is transparent and what is an actual pixel, now as of right now it
    the question is if the transparent pictures are even being red out but I
    would assume so, as there is nothing that would indicate otherwise from
    printing as per:
    
    BBBBBBBBBBBBBBBB
    BBBBB      BBBBB
    BBBBB WBBW  BBBB
    BBBB W BB BBBBBB
    BBBB W  W B WBBB
    BBB W  WW B  WBB
    BB W WW    BBBBB
    B W W  BBBBBBBBB
     W W BB    BBBBB
     WW B  WWW BBBBB
     WW B WWWW BBBBB
     WW B     BBBBBB
     WW  BBBBBBBBBBB
    B  B    BBBBBBBB
    BBBBBBBBBBBBBBBB
    BBBBBBBBBBBBBBBB
    
    which should not have some of the parts as it was the bird.png used in
    there

--- tools/rust_png_to_svg/src/lib.rs
+++ tools/rust_png_to_svg/src/lib.rs
@@ -1,5 +1,7 @@
 pub fn get_color(color: u8) -> String {
-    if color < 40 {
+    if color < 1 {
+        String::from("SKIP")
+    } else if color < 40 {
         String::from("\\outline\\")
     } else if color > 40 && color < 80 {
         String::from("\\darkspot\\")
@@ -15,17 +17,19 @@ pub fn get_color(color: u8) -> String {
 }
 
 pub fn get_symbol_for_terminal(color: u8) -> String {
-    if color < 40 {
-        String::from("#")
-    } else if color > 40 && color < 80 {
-        String::from("X")
-    } else if color > 80 && color < 118 {
-        String::from("$")
-    } else if color > 125 && color < 180 {
-        String::from("Z")
-    } else if color > 180 && color < 220 {
-        String::from("U")
-    } else {
+    if color < 1 {
         String::from(" ")
+    } else if color < 40 {
+        String::from("B")
+    } else if color > 40 && color < 80 {
+        String::from("\"")
+    } else if color > 80 && color < 118 {
+        String::from("%")
+    } else if color > 125 && color < 180 {
+        String::from("\'")
+    } else if color > 180 && color < 220 {
+        String::from("$")
+    } else {
+        String::from("W")
     }
 }
--- tools/rust_png_to_svg/src/main.rs
+++ tools/rust_png_to_svg/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     let res = image::open(path_to_img);
     if let Ok(img) = res {
         let dimensions = img.dimensions();
-        println!("Dimensions {:?}", img.dimensions());
+        println!("Dimensions {:?}", dimensions);
         if dimensions.0 != 16 && dimensions.1 != 16 {
             println!("Error: Wrong image my guy, here we work on 16 by 16");
             println!("Info: Should it be an error? No. Do I care? Not enough");
@@ -16,9 +16,12 @@ fn main() {
         for one in img.pixels().enumerate() {
             let access_rgba = one.1.2;
             let actuall_rgb_pixel = access_rgba.to_rgb();
-            let result = get_color(actuall_rgb_pixel.0[0]);
-            // let result = get_symbol_for_terminal(actuall_rgb_pixel.0[0]);
-            print!("{:?}", result);
+            // let result = get_color(actuall_rgb_pixel.0[0]);
+            let result = get_symbol_for_terminal(actuall_rgb_pixel.0[0]);
+            print!("{}", result);
+            if one.0 % 16 == 0 {
+                println!();
+            }
         }
     } else {
         println!("Error: It uses relative path to find image {}", path_to_img);
