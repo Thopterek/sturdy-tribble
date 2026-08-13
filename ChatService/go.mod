c277c6995ca17a34538a206c776fb56d6e8d34f1
SPARES888 <sty.papas1@gmail.com>
2026-08-13 15:41:52 +0200

    Hello Guys i'm back

--- ChatService/go.mod
+++ ChatService/go.mod
@@ -1,3 +1,5 @@
 module chat-service
 
 go 1.26.5
+
+require github.com/gorilla/websocket v1.5.3
--- ChatService/main.go
+++ ChatService/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
 
 func main() {
-	fmt.Println("ChatService lauft")
+	http.HandleFunc("/ChatHealth", healthHandler)
+	http.HandleFunc("/ws", websocketHandler)
+
+	fmt.Println("ChatService lauft auf Port 8081")
+
+	err := http.ListenAndServe(":8081", nil)
+	if err != nil {
+		fmt.Println("Serverfehler:", err)
+	}
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "ChatService ist erreichbar")
+}
+
+func websocketHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println("Websocket-Fehler:", err)
+		return
+	}
+
+	defer conn.Close()
+
+	fmt.Println("WebSocket verbunden")
 }
