8be1a4a86e42d82c5f3f2cbba83ce4900af442d6
SPARES888 <sty.papas1@gmail.com>
2026-08-14 13:03:47 +0200

    Added now first client using

--- ChatService/main.go
+++ ChatService/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync"
 
 	"github.com/gorilla/websocket"
 )
@@ -11,6 +12,14 @@ type Message struct {
 	RecipientID string `json:"recipient_id"`
 	Content string `json:"content"`
 }
+
+type Client struct {
+	ID string
+	Conn *websocket.Conn
+}
+
+var clients = make(map[string]*Client)
+var clientsMutex sync.RWMutex
 
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
@@ -35,6 +44,15 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func websocketHandler(w http.ResponseWriter, r *http.Request) {
+	userID := r.URL.Query().Get("user_id")
+
+	if userID == "" {
+		http.Error(w, "user_id fehlt", http.StatusBadRequest)
+		return
+	}
+
+	fmt.Println("Verbindungsanfrage von:", userID)
+
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		fmt.Println("Websocket-Fehler:", err)
