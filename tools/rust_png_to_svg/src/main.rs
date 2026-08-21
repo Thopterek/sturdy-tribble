6a7077c524e48e11a84620c8b9427f6a3fe386ba
Thopterek <nataniel.dziadzia@gmail.com>
2026-08-21 22:48:11 +0200

    It's getting late, I need to remember how to access types in Rust
    * re learning once again couple of things as of right now it is a bit
    too messy without just trying to figure out the logic without making a
    whole different logical structure which should be just a simple while
    loop with offset and checking if we are keeping the boundries
    
    * that was all the points, bye

--- tools/rust_png_to_svg/src/main.rs
+++ tools/rust_png_to_svg/src/main.rs
@@ -1,21 +1,31 @@
+use std::usize;
+
 use image::{DynamicImage, GenericImageView};
 
 use rust_png_to_svg::c_to_char;
 #[cfg(debug_assertions)]
 use rust_png_to_svg::print_png_on_terminal;
 
+fn double_check_failed(img: &DynamicImage) -> bool {
+    let dimensions = img.dimensions();
+    println!("Dimensions {:?}", dimensions);
+    if dimensions.0 != 16 && dimensions.1 != 16 {
+        println!("Error: Wrong image my guy, here we work on 16 by 16");
+        println!("Info: Should it be an error? No. Do I care? Not enough");
+        return true;
+    }
+    false
+}
+
 fn main() {
     let path_to_img = "../../overview/pixel_art_sketches/bird.png";
     let res = image::open(path_to_img);
     if let Ok(img) = res {
-        let dimensions = img.dimensions();
-        println!("Dimensions {:?}", dimensions);
-        if dimensions.0 != 16 && dimensions.1 != 16 {
-            println!("Error: Wrong image my guy, here we work on 16 by 16");
-            println!("Info: Should it be an error? No. Do I care? Not enough");
+        if double_check_failed(&img) {
             return;
         }
-        println!("horizontal rec size {}", horizontal_rectangle(&img));
+        println!("horizontal rec size {}", horizontal_rectangle(&img, 0));
+        println!("vertical rec size {}", vertical_rectangle(&img, 0));
         #[cfg(debug_assertions)]
         print_png_on_terminal(img);
         //print_rgba(img);
@@ -28,11 +38,18 @@ fn debug_vals(c: char, size: usize, offset: usize) {
     println!("{}, size {}, offset {}", c, size, offset);
 }
 
-fn horizontal_rectangle(img_slice: &DynamicImage) -> usize {
+/*
+* I am not handling two lines of same color because I didn't draw any
+* if it would be actually useful I should probably add it, just like many other things
+*/
+fn horizontal_rectangle(img: &DynamicImage, start: usize) -> usize {
     let mut size: usize = 1;
     let mut offset: usize = 0;
     let mut first_pixel: char = 'N';
-    for (counter, (_w, _h, pixel_data)) in img_slice.pixels().enumerate() {
+    for (mut counter, (_w, _h, pixel_data)) in img.pixels().enumerate() {
+        if counter == 0 {
+            counter = start;
+        }
         if pixel_data[3] >= 127 {
             size = counter + 1;
             debug_vals(c_to_char(pixel_data[0]), size, offset);
@@ -51,34 +68,18 @@ fn horizontal_rectangle(img_slice: &DynamicImage) -> usize {
     size - offset
 }
 
-/*
-fn vertical_rectangle(img: &DynamicImage) -> (usize) {
-    0
-}
-*/
-
-/*
-* Actually change of idea let's to do it easier
-fn find_rectangle(img_slice: &DynamicImage) -> (usize, usize) {
-    let mut end_x: usize = 0;
-    let mut end_y: usize = 0;
-    let mut n_pixel: String;
-    let mut last_p: String;
-    let mut pixel_changed = false;
-    for (counter, pixel_data) in img_slice.pixels().enumerate() {
-        if pixel_data.2[4] >= 127 {
-            n_pixel = get_symbol_for_terminal(pixel_data.2[0]);
-            if n_pixel != last_p {
-                pixel_changed = true;
+fn vertical_rectangle(img: &DynamicImage, start: usize) -> usize {
+    let mut size: usize = 0;
+    let mut f_pixel: char = 'N';
+    let mut current_p: char = 'N';
+    while f_pixel == current_p {
+        for (counter, (_w, _h, pixel_data)) in img.pixels().enumerate() {
+            current_p = c_to_char(pixel_data[0]);
+            if (f_pixel == 'N') {
+                f_pixel = c_to_char(pixel_data[0]);
             }
         }
-        end_x = counter - (end_y * 15);
-        if (counter + 1) % 16 == 0 {
-            end_y += 1;
-            #[cfg(debug_assertions)]
-            print!("next_line");
-        }
+        size += 1;
     }
-    (end_x, end_y)
+    size
 }
-*/
