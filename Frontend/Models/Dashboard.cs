9141d5604d3f6f77d0e0b3491f6de59f7c0be4e0
Thopterek <nataniel.dziadzia@gmail.com>
2026-09-06 20:48:13 +0200

    A lot more needs to come but it shows online status
    * now if that online status would be kept is another thing as for one it
    is some extra requests that are being sent around for no good reason
    really as there is no chat option anyway to care about so communication
    is going through something else till you get to this point anyway
    
    * as for the hours and also the general style that is something that
    would be great to rework as of right now it is just kinda meh?

--- Frontend/Models/Dashboard.cs
+++ Frontend/Models/Dashboard.cs
@@ -2,6 +2,10 @@ namespace Frontend.Models;
 
 public record GameLobby(string Name, string System, string[] Players);
 
-public record Friend(string Name, string IdkNow);
+public record Friend(string Name, bool Online, int Hours);
 
-public record DashboardData(IReadOnlyList<GameLobby> Games, IReadOnlyList<Friend> Friends);
+public record DashboardData(
+    IReadOnlyList<GameLobby> Games,
+    IReadOnlyList<Friend> Friends,
+    Friend User
+);
